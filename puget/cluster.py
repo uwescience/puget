62fb2621bc7d5fd2085826fe4090be40fbe15efb
arokem <arokem@gmail.com>
2016-01-18 15:12:53 -0800

    First pass at the clustering on group variable.

--- puget/cluster.py
+++ puget/cluster.py
@@ -1,5 +1,6 @@
+"""
 
-
+"""
 import numpy as np
 import pandas as pd
 import itertools
@@ -10,11 +11,31 @@ try:
 except ImportError:
     from scipy.cluster.hierarchy import linkage
 
-import scipy.sparse as sps
+from scipy.cluster.hierarchy import fcluster
 
-def groups_distance(df, group_var, individual_var, T=None):
+
+def make_mapping(unique_individuals):
     """
-    Calculate distances based a group
+    Create a mapping between a set of unique indivdual id's and indices into
+    co-occurence and distance matrices.
+    """
+    mapping = OrderedDict(zip(unique_individuals,
+                          np.arange(unique_individuals.shape[0])))
+    inv_mapping = OrderedDict((value, key)
+                              for key, value in mapping.items())
+    return mapping, inv_mapping
+
+
+def groups_co_occurence(df, group_var, individual_var, T=None,
+                        mapping=None):
+    """
+    Count the co-occurence of individuals in a group.
+
+    Returns
+    -------
+    Matrix with integer values that indicates the number of times individuals
+    (mapped through mapping and inv_mapping) have appeared together in the
+    same group.
     """
     # Filter to non-null client_key and non-null group_key
     df = df[pd.notnull(df[individual_var])]
@@ -22,15 +43,12 @@ def groups_distance(df, group_var, individual_var, T=None):
 
     unique_individuals = df[individual_var].unique()
     if T is None:
-        T = sps.csr_matrix((unique_individuals.shape[0],
-                            unique_individuals.shape[0]))
-
-    # Make dictionaries mapping    matrix indices <--> ClientID's
-    mapping = OrderedDict(zip(unique_individuals,
-                          np.arange(unique_individuals.shape[0])))
-
-    inv_mapping = OrderedDict((value, key)
-                              for key, value in mapping.iteritems())
+        T = np.zeros((unique_individuals.shape[0],
+                      unique_individuals.shape[0]))
+    if mapping is None:
+        mapping, inv_mapping = make_mapping(unique_individuals)
+    else:
+        mapping, inv_mapping = mapping
 
     gb = df.groupby(group_var)
     for gid, group in gb:
@@ -46,8 +64,59 @@ def groups_distance(df, group_var, individual_var, T=None):
     return T
 
 
-def distance_matrix():
+def time_co_occurence(df, time_var, individual_var, T=None,
+                      mapping=None):
     """
-    Calculate the full distance matrix.
+    Group by co-occurence of the times of enrollment.
     """
-    pass
+    raise NotImplementedError
+
+
+def linkage_matrix(T, method='complete'):
+    """
+    Calculate a linkage matrix from a co-occurence matrix
+
+    Parameters
+    ----------
+    T : co-occurence matrix
+    """
+    D = np.zeros_like(T)
+
+    # Distances are the inverse of co-occurences:
+    D[T > 0] = 1.0 / T[T > 0]
+    # No co-occurence translates into a distance of 2 (which is equivalent to
+    # infinity for our purposes):
+    D[T == 0] = 2
+    # Distance self-to-self is 0:
+    np.fill_diagonal(D, 0)
+    Z = linkage(D, method=method)
+    return Z
+
+
+def cluster(df, group_var, individual_var):
+    """
+    Calculate clusters from a co-occurence matrix
+
+    Parameters
+    ----------
+    df : DataFrame
+
+    """
+    # Filter to non-null client_key and non-null group_key
+    df = df[pd.notnull(df[individual_var])]
+    df = df[pd.notnull(df[group_var])]
+
+    unique_individuals = df[individual_var].unique()
+
+    T = np.zeros((unique_individuals.shape[0],
+                  unique_individuals.shape[0]))
+
+    mapping, inv_mapping = make_mapping(unique_individuals)
+    T = groups_co_occurence(df, group_var, individual_var, T=T,
+                            mapping=[mapping, inv_mapping])
+
+    Z = linkage_matrix(T)
+    clusters = fcluster(Z, t=1.01)
+
+    df['cluster'] = df[individual_var].apply(lambda x: clusters[mapping[x]])
+    return df
--- puget/tests/test_cluster.py
+++ puget/tests/test_cluster.py
@@ -7,11 +7,46 @@ import pandas.util.testing as pdt
 
 import puget.cluster as cluster
 
-def test_groups_distance():
 
+def test_make_mapping():
+    pass
+
+
+def test_cluster_by_groups():
+    # In the first case, all individuals are linked through a chain of
+    # co-occurences:
     df1 = pd.DataFrame({'individual_var': [1, 2, 3, 4, 1, 2, 3, 4],
-                             'group_var': [1, 1, 2, 2, 1, 2, 1, 2]})
+                        'group_var': [1, 1, 2, 2, 1, 2, 1, 2]})
 
-    T = cluster.groups_distance(df1, 'group_var', 'individual_var')
+    T = cluster.groups_co_occurence(df1, 'group_var', 'individual_var')
     true_T = np.array([[0, 1, 1, 0], [1, 0, 2, 1], [1, 2, 0, 1], [0, 1, 1, 0]])
-    npt.assert_equal(T.toarray(), true_T)
+    npt.assert_equal(T, true_T)
+
+    df1_out = cluster.cluster(df1, 'group_var', 'individual_var')
+    true_df1_out = pd.DataFrame({'individual_var': [1, 2, 3, 4, 1, 2, 3, 4],
+                                 'group_var': [1, 1, 2, 2, 1, 2, 1, 2],
+                                 'cluster': [1, 1, 1, 1, 1, 1, 1, 1]})
+
+    pdt.assert_frame_equal(df1_out.sort_index(axis=1),
+                           true_df1_out.sort_index(axis=1))
+
+
+
+    # In the second case, individuals are unlinked into two clusters:
+    df2 = pd.DataFrame({'individual_var': [1, 2, 3, 4, 1, 2, 3, 4],
+                        'group_var': [1, 1, 3, 3, 1, 1, 3, 3]})
+
+    T = cluster.groups_co_occurence(df2, 'group_var', 'individual_var')
+    true_T = np.array([[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])
+    T = cluster.groups_co_occurence(df2, 'group_var', 'individual_var')
+    npt.assert_equal(T, true_T)
+
+    # Cluster's are designated as [1, 2, 3, ...], even while the group variable
+    # can have arbitrary values:
+    df2_out = cluster.cluster(df2, 'group_var', 'individual_var')
+    true_df2_out = pd.DataFrame({'individual_var': [1, 2, 3, 4, 1, 2, 3, 4],
+                                 'group_var': [1, 1, 3, 3, 1, 1, 3, 3],
+                                 'cluster': [1, 1, 2, 2, 1, 1, 2, 2]})
+
+    pdt.assert_frame_equal(df2_out.sort_index(axis=1),
+                           true_df2_out.sort_index(axis=1))
