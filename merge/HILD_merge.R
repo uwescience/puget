756bf6636d617fd0c4e925234b0a7c6c2a963adc
jmhernan <jmhernandeze@gmail.com>
2018-12-04 12:28:07 -0800

    added package handling and commented out linkage

--- merge/HILD_merge.R
+++ merge/HILD_merge.R
@@ -4,37 +4,52 @@
 # ==========================================================================
 
 	rm(list=ls()) #reset
-	options(max.print = 10000,
-		    tibble.print_max = 100,
-		    scipen = 999,
-		    width = 78, # adjust for terminal screen size
-		    error = traceback)
-	gc()
+	# options(max.print = 10000,
+	# 	    tibble.print_max = 100,
+	# 	    scipen = 999,
+	# 	    width = 78, # adjust for terminal screen size
+	# 	    error = traceback)
+	# gc()
 
 # ==========================================================================
 # Library
 # ==========================================================================
+if(!require(devtools)){
+	    install.packages("devtools")
+	    require(devtools)
+	}
 
-	library(colorout) # colorize code in terminal
-	library(gdata) # keep function:
-	library(data.table)
-	library(RecordLinkage)
-	library(lubridate)
-	library(tidyverse)
+if(!require(gdata)){
+	    install.packages("gdata")
+	    require(gdata)
+	}
 
+if(!require(data.table)){
+	install.packages("data.table")
+	require(data.table)
+}
+
+if(!require(tidyverse)){
+	devtools::install_github("tidyverse/tidyverse")
+	require(tidyverse)
+}
+
+if(!require(lubridate)){
+	    devtools::install_github("tidyverse/lubridate")
+	    require(lubridate)
+	}
 # ==========================================================================
 # Data pull
 # ==========================================================================
 
 	path <- "/home/ubuntu/data"
 
-	hmis <- fread(paste0(path,"/HMIS/puget_preprocessed.csv")) %>%
+	hmis <- fread(paste0(path,"/HMIS/2016/puget_preprocessed.csv")) %>%
 			mutate(pid0 = paste("HMIS0_",PersonalID,sep=""))
 
-	load(paste0(path,"/Housing/OrganizedData/pha_longitudinal.Rdata"))
-
-	pha <- pha_longitudinal %>%
-		   mutate(pid0 = paste("PHA0_",pid, sep = ""))
+	pha <- fread(paste0(path,"/HILD/pha_longitudinal.csv")) %>%
+			mutate(pid0 = paste("PHA0_",pid, sep = ""))
+	
 
 # ==========================================================================
 # Data prep
@@ -92,9 +107,9 @@
 # Change "" to NA
 	pha.rl[pha.rl==""] <- NA
 
-# Convert DOB to Date
-	pha.rl <- pha.rl %>%
-			  mutate(dob = ymd(dob))
+# # Convert DOB to Date
+# 	pha.rl <- pha.rl %>%
+# 			  mutate(dob = ymd(dob))
 
 # Subset HMIS
 	hmis.rl <- hmis %>%
@@ -126,8 +141,8 @@
 	hmis.rl[hmis.rl==""] <- NA
 
 # Mutate dob to date
-	hmis.rl <- hmis.rl %>%
-			   mutate(dob=ymd(dob))
+	# hmis.rl <- hmis.rl %>%
+	# 		   mutate(dob=ymd(dob))
 
 # combine
 	df <- bind_rows(pha.rl,hmis.rl) %>%
@@ -163,8 +178,6 @@
 							!grepl("ANONYMOUS",lname),
 							!grepl("ANONYMOUS",fname))
 
-	gc()
-
 	#=======================================================================
 	# Codebook: PID's and SSN's
 	# pid0 = personal ID from pha and hmis - Alastair's linkage and HMIS ID's
@@ -183,177 +196,176 @@
 # Linkage Function Creation
 # ==========================================================================
 
-	rlink <- function(df,
-					  block,
-					  string,
-					  phonetic,
-					  phonfun = soundex,
-					  ex = ex,
-					  threshold = 0.1,
-					  name_wt = paste0(block,"_wt")){
+	# rlink <- function(df,
+	# 				  block,
+	# 				  string,
+	# 				  phonetic,
+	# 				  phonfun = soundex,
+	# 				  ex = ex,
+	# 				  threshold = 0.1,
+	# 				  name_wt = paste0(block,"_wt")){
 
-		ex <- names(df)[!names(df) %in% string]
-		RL <- df %>%
-				compare.dedup(.,
-				  			  blockfld = block,
-				  			  strcmp = string[!string %in% block],
-				  			  phonetic = phonetic,
-				  			  phonfun = phonfun,
-				  			  exclude = ex) %>%
-				epiWeights(.) %>%
-				epiClassify(.,threshold) %>%
-				getPairs(., show = "links", single.rows = TRUE) %>%
-				arrange(id1) %>%
-				filter(!duplicated(id2)) %>% # reduce duplicated id's
-				select(ssn.1:ssn_dq.1,
-					   pid0.1,
-					   pid0.2,
-					   pid1.1,
-					   pid1.2,
-					   pid2.1,
-					   pid2.2,
-					   Weight) %>%
-				rename_(.dots = setNames("Weight", name_wt))
+	# 	ex <- names(df)[!names(df) %in% string]
+	# 	RL <- df %>%
+	# 			compare.dedup(.,
+	# 			  			  blockfld = block,
+	# 			  			  strcmp = string[!string %in% block],
+	# 			  			  phonetic = phonetic,
+	# 			  			  phonfun = phonfun,
+	# 			  			  exclude = ex) %>%
+	# 			epiWeights(.) %>%
+	# 			epiClassify(.,threshold) %>%
+	# 			getPairs(., show = "links", single.rows = TRUE) %>%
+	# 			arrange(id1) %>%
+	# 			filter(!duplicated(id2)) %>% # reduce duplicated id's
+	# 			select(ssn.1:ssn_dq.1,
+	# 				   pid0.1,
+	# 				   pid0.2,
+	# 				   pid1.1,
+	# 				   pid1.2,
+	# 				   pid2.1,
+	# 				   pid2.2,
+	# 				   Weight) %>%
+	# 			rename_(.dots = setNames("Weight", name_wt))
 
-		return(RL)
-	}
+	# 	return(RL)
+	# }
 
-	gc()
+	# gc()
 
 # ==========================================================================
 # Record Linkages
 # ==========================================================================
 
-### Fields
-	s <- c( "ssn1","dob1_d","dob1_m","dob_y","fname","mname","lname","suf")
-	p <- c("lname", "fname", "mname")
+# ### Fields
+# 	s <- c( "ssn1","dob1_d","dob1_m","dob_y","fname","mname","lname","suf")
+# 	p <- c("lname", "fname", "mname")
 
-### ssn1 RL
-	system.time(
-		rl_ssn1 <- rlink(df = df_sub,
-			block = "ssn1",
-			string = s,
-			phonetic = p
-			))
-	gc()
+# ### ssn1 RL
+# 	system.time(
+# 		rl_ssn1 <- rlink(df = df_sub,
+# 			block = "ssn1",
+# 			string = s,
+# 			phonetic = p
+# 			))
+# 	gc()
 
-### dob1_ym RL
-	system.time(
-		rl_dob1_ym <- rlink(df = df_sub,
-			block = c("dob_y","dob1_m"),
-			string = s,
-			phonetic = p,
-			name_wt = "ym_wt"
-			))
-	gc()
+# ### dob1_ym RL
+# 	system.time(
+# 		rl_dob1_ym <- rlink(df = df_sub,
+# 			block = c("dob_y","dob1_m"),
+# 			string = s,
+# 			phonetic = p,
+# 			name_wt = "ym_wt"
+# 			))
+# 	gc()
 
-### fname RL
-	system.time(
-		rl_fname <- rlink(df = df_sub,
-			block = c("fname"),
-			string = s,
-			phonetic = p,
-			threshold = .1,
-			name_wt = "fname_wt"
-			))
-	gc()
+# ### fname RL
+# 	system.time(
+# 		rl_fname <- rlink(df = df_sub,
+# 			block = c("fname"),
+# 			string = s,
+# 			phonetic = p,
+# 			threshold = .1,
+# 			name_wt = "fname_wt"
+# 			))
+# 	gc()
 
-### lname RL
-	system.time(
-		rl_lname <- rlink(df = df_sub,
-			block = c("lname"),
-			string = s,
-			phonetic = p,
-			threshold = .1,
-			name_wt = "lname_wt"
-			))
-	gc()
+# ### lname RL
+# 	system.time(
+# 		rl_lname <- rlink(df = df_sub,
+# 			block = c("lname"),
+# 			string = s,
+# 			phonetic = p,
+# 			threshold = .1,
+# 			name_wt = "lname_wt"
+# 			))
+# 	gc()
 
 # ==========================================================================
 # Create link df
 # ==========================================================================
 
 ### Join links
-	link <- rl_ssn1 %>%
-			select(ssn.1:ssn_dq.1,
-				   pid0.1,
-				   pid0.2,
-				   pid1.1,
-				   pid1.2,
-				   pid2.1,
-				   pid2.2,
-				   ends_with("_wt"))
+# 	link <- rl_ssn1 %>%
+# 			select(ssn.1:ssn_dq.1,
+# 				   pid0.1,
+# 				   pid0.2,
+# 				   pid1.1,
+# 				   pid1.2,
+# 				   pid2.1,
+# 				   pid2.2,
+# 				   ends_with("_wt"))
 
-	link <- full_join(link,
-					  rl_dob1_ym %>%
-			select(pid2.1,
-				   pid2.2,
-				   ends_with("_wt")))
+# 	link <- full_join(link,
+# 					  rl_dob1_ym %>%
+# 			select(pid2.1,
+# 				   pid2.2,
+# 				   ends_with("_wt")))
 
-	link <- full_join(link,
-					  rl_fname %>%
-			select(pid2.1,
-				   pid2.2,
-				   ends_with("_wt")))
+# 	link <- full_join(link,
+# 					  rl_fname %>%
+# 			select(pid2.1,
+# 				   pid2.2,
+# 				   ends_with("_wt")))
 
-	link <- full_join(link,
-					  rl_lname %>%
-			select(pid2.1,
-				   pid2.2,
-				   ends_with("_wt")))
+# 	link <- full_join(link,
+# 					  rl_lname %>%
+# 			select(pid2.1,
+# 				   pid2.2,
+# 				   ends_with("_wt")))
 
-### Change NA to 0
-	link <- link %>%
-			mutate_at(vars(ends_with("wt")),
-					 	   funs(ifelse(is.na(.)==T,
-					  	   0.0,
-					  	   .))
-					  )
-	glimpse(link)
+# ### Change NA to 0
+# 	link <- link %>%
+# 			mutate_at(vars(ends_with("wt")),
+# 					 	   funs(ifelse(is.na(.)==T,
+# 					  	   0.0,
+# 					  	   .))
+# 					  )
+# 	glimpse(link)
 
 # ==========================================================================
 # Create product weights
 # ==========================================================================
 
 ### Weighting function
-	wtp <- function(p,w){
-			# 1-prod((1-p)^w)^(1/sum(w))
-			1-(prod((1-p)^w))^(1/(sum(w)))
-		}
+	# wtp <- function(p,w){
+	# 		# 1-prod((1-p)^w)^(1/sum(w))
+	# 		1-(prod((1-p)^w))^(1/(sum(w)))
+	# 	}
 
 ### Different weighting scenerios
-	w1 <- link %>%
-		mutate(wtp1111 = apply( # apply function across rows
-						select(., ends_with("wt")), # select columns to apply to
-						1, # w/in each row (2 would do it w/in each col)
-						wtp, # function to apply
-						w=c(1,1,1,1)), # w argument
-			   wtp1.8.8.8 = apply(
-						select(., ends_with("wt")),
-						1,
-						wtp,
-						w=c(1,.8,.8,.8)),
-			   wtp1.5.5.5 = apply(
-						select(., ends_with("wt")),
-						1,
-						wtp,
-						w=c(1,.5,.5,.5))
-						)
+	# w1 <- link %>%
+	# 	mutate(wtp1111 = apply( # apply function across rows
+	# 					select(., ends_with("wt")), # select columns to apply to
+	# 					1, # w/in each row (2 would do it w/in each col)
+	# 					wtp, # function to apply
+	# 					w=c(1,1,1,1)), # w argument
+	# 		   wtp1.8.8.8 = apply(
+	# 					select(., ends_with("wt")),
+	# 					1,
+	# 					wtp,
+	# 					w=c(1,.8,.8,.8)),
+	# 		   wtp1.5.5.5 = apply(
+	# 					select(., ends_with("wt")),
+	# 					1,
+	# 					wtp,
+	# 					w=c(1,.5,.5,.5))
+	# 					)
 
 ### View distributions
 
-	head(w1) # weights all at 1
-	hist(w1$wtp111, breaks = 40)
-	hist(w1$wtp1.8.8, breaks = 40)
-	hist(w1$wtp1.5.5, breaks = 40)
+	# head(w1) # weights all at 1
+	# hist(w1$wtp111, breaks = 40)
+	# hist(w1$wtp1.8.8, breaks = 40)
+	# hist(w1$wtp1.5.5, breaks = 40)
 
 # ==========================================================================
 # Save file
 # ==========================================================================
 
-	write.csv(w1, paste0(path,"/Housing/links/WeightedLinks.csv"))
-	write.csv(df_sub, paste0(path,"/Housing/links/PreLinkData.csv"))
-
+	# write.csv(w1, paste0(path,"/Housing/links/WeightedLinks.csv"))
+	write.csv(df_sub, paste0(path,"HILD/PreLinkData_test.csv"))
 
 # ==========================================================================
 # End code
